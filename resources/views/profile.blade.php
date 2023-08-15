5b8f69ee857aad8fe4306de821f8e43330ef9353
Yoma90 <yomataniel@gmail.com>
2023-08-15 16:44:24 +0100

    views

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,13 +11,12 @@
         <div class="row gx-4">
           <div class="col-auto">
             <div class="avatar avatar-xl position-relative">
-              <img src="../assets/img/bruce-mars.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+              <img src="../assets/img/kempes.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
             </div>
           </div>
           <div class="col-auto my-auto">
             <div class="h-100">
               <h5 class="mb-1">
-                Alec Thompson
               </h5>
               <p class="mb-0 font-weight-bold text-sm">
                 CEO / Co-Founder
