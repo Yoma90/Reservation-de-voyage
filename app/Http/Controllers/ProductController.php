0310bdce412b0ea1ff78a729767f73bce6784890
Yoma90 <yomataniel@gmail.com>
2023-11-17 18:42:57 +0100

    done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -94,6 +94,9 @@ class ProductController extends Controller
             [
                 [
                     'src' => 'https://veroniquecloutier.com/famille/votre-chien-sennuie-t-il'
+                ],
+                [
+                    'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
                 ]
             ];
         $attributes['type'] = "simple";
